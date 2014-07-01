f7b0f7c6c673983bd1cf66a093ab5d0f439860b9
grigoriy.trofimyuk@gmail.com <grigoriy.trofimyuk@gmail.com@6788df26-0534-eb5a-cd33-def53b161572>
2014-07-01 16:55:58 +0000

    Added primary client-server exchange interfaces and classes

---
+++ src/main/java/roi/students/t3t/shared/dao/Response.java
@@ -0,0 +1,8 @@
+package roi.students.t3t.shared.dao;
+
+import java.util.List;
+
+public interface Response {
+	public List<HotelResponse> getHotelResponse();
+	public HotelRequest getHotelRequest();
+}
