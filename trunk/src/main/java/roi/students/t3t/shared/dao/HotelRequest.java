3c14b9c3798f17a51352f304782be7af4aa33670
grigoriy.trofimyuk@gmail.com <grigoriy.trofimyuk@gmail.com@6788df26-0534-eb5a-cd33-def53b161572>
2014-07-02 17:05:53 +0000

    Added simple HotelRequest and HotelInfo implementations

--- trunk/src/main/java/roi/students/t3t/shared/dao/HotelRequest.java
+++ trunk/src/main/java/roi/students/t3t/shared/dao/HotelRequest.java
@@ -7,8 +7,8 @@ public interface HotelRequest {
 	public Date getStartDate();
 	public Date getFinishDate();
 	public String getCountry();
-	public String getMinStars();
-	public String getMaxStars();
+	public int getMinStars();
+	public int getMaxStars();
 	public TypeFood getTypeFood();
 	public int getMinPrice();
 	public int getMaxPrice();
