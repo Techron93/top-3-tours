3aede78005cb0ebae71c670e7fcda941f58df6ba
grigoriy.trofimyuk@gmail.com <grigoriy.trofimyuk@gmail.com@6788df26-0534-eb5a-cd33-def53b161572>
2014-07-02 17:05:53 +0000

    Added simple HotelRequest and HotelInfo implementations

---
+++ src/main/java/roi/students/t3t/shared/dao/impl/HotelInfoImpl.java
@@ -0,0 +1,52 @@
+package roi.students.t3t.shared.dao.impl;
+
+import roi.students.t3t.shared.dao.HotelInfo;
+
+public class HotelInfoImpl implements HotelInfo{
+	
+	private String url;
+	private int price;
+	private String name;
+	private int stars;
+	
+	public HotelInfoImpl(){
+		url = null;
+		price = 0;
+		name = null;
+		stars  = 0;
+	}
+	
+	public HotelInfoImpl(String url, int price, String name, int stars){
+		setUrl(url);
+		setPrice(price);
+		setStars(stars);
+		setName(name);
+	}
+	
+	public String getURL() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getStars() {
+		return stars;
+	}
+	public void setStars(int stars) {
+		this.stars = stars;
+	}
+	
+
+}
---
+++ src/main/java/roi/students/t3t/shared/dao/impl/HotelRequestImpl.java
@@ -0,0 +1,102 @@
+package roi.students.t3t.shared.dao.impl;
+
+import java.util.Date;
+
+import roi.students.t3t.shared.dao.HotelRequest;
+import roi.students.t3t.shared.dao.TypeFood;
+
+public class HotelRequestImpl implements HotelRequest {
+	
+	private Date startDate;
+	private Date finishDate;
+	private String country;
+	private int minStars;
+	private int maxStars;
+	private TypeFood typeFood;
+	//дополнительные параметры по умолчанию
+	private int minPrice = 0;
+	private int maxPrice = 0;
+	private int peopleCount = 0; //TODO: сделать enum на основе предложений сайтов
+	
+	public HotelRequestImpl() {
+		startDate = new Date(); //текущая дата
+		finishDate = new Date();
+		country = null;
+	}
+	
+	public HotelRequestImpl(Date startDate, Date finishDate, int minStars, int maxStars){
+		setStartDate(startDate);
+		setFinishDate(finishDate);
+		setMinStars(minStars);
+		setMaxStars(maxStars);
+	}
+	
+	public HotelRequestImpl(Date startDate, Date finishDate, String country, int minStars, int maxStars, TypeFood typeFood, 
+			int minPrice, int maxPrice, int peopleCount){
+		
+		setStartDate(startDate);
+		setFinishDate(finishDate);
+		setCountry(country);
+		setMinStars(minStars);
+		setMaxStars(maxStars);
+		setTypeFood(typeFood);
+		setMinPrice(minPrice);
+		setMaxPrice(maxPrice);
+		setPeopleCount(peopleCount);
+	}
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getFinishDate() {
+		return finishDate;
+	}
+	public void setFinishDate(Date finishDate) {
+		this.finishDate = finishDate;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public int getMinStars() {
+		return minStars;
+	}
+	public void setMinStars(int minStars) {
+		this.minStars = minStars;
+	}
+	public int getMaxStars() {
+		return maxStars;
+	}
+	public void setMaxStars(int maxStars) {
+		this.maxStars = maxStars;
+	}
+	public TypeFood getTypeFood() {
+		return typeFood;
+	}
+	public void setTypeFood(TypeFood typeFood) {
+		this.typeFood = typeFood;
+	}
+	public int getMinPrice() {
+		return minPrice;
+	}
+	public void setMinPrice(int minPrice) {
+		this.minPrice = minPrice;
+	}
+	public int getMaxPrice() {
+		return maxPrice;
+	}
+	public void setMaxPrice(int maxPrice) {
+		this.maxPrice = maxPrice;
+	}
+	public int getPeopleCount() {
+		return peopleCount;
+	}
+	public void setPeopleCount(int peopleCount) {
+		this.peopleCount = peopleCount;
+	}
+}
