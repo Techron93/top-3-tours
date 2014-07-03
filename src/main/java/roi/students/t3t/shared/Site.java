e8541ba69005b0e9b7cb7d9f7f22ca8391d8b3ba
Anatoly.Kondratyev@gmail.com <Anatoly.Kondratyev@gmail.com@6788df26-0534-eb5a-cd33-def53b161572>
2014-07-03 09:33:47 +0000

    How enums should be used

---
+++ src/main/java/roi/students/t3t/shared/Site.java
@@ -0,0 +1,15 @@
+package roi.students.t3t.shared;
+
+public enum Site {
+	teztour("http://www.tez-tour.com/","TEZ tour"), 
+	itour("http://itour.ru/","itour");
+		
+	public final String url;
+	public final String name;
+	
+	Site(String url, String name){
+		this.url = url;
+		this.name = name;
+	}
+
+}
