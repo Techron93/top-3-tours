cf2be7c034327e021ff3b73a7aabf1d77867e38e
nick.yakuba@gmail.com <nick.yakuba@gmail.com@6788df26-0534-eb5a-cd33-def53b161572>
2014-07-04 08:44:43 +0000

    Test layout elements.

--- trunk/src/main/java/roi/students/t3t/client/OptionsPanel.java
+++ trunk/src/main/java/roi/students/t3t/client/OptionsPanel.java
@@ -23,25 +23,18 @@ public class OptionsPanel extends Composite implements HasText {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 
-	@UiField
-	Button button;
-
 	public OptionsPanel(String firstName) {
 		initWidget(uiBinder.createAndBindUi(this));
-		button.setText("Options");
-	}
-
-	@UiHandler("button")
-	void onClick(ClickEvent e) {
-		Window.alert("Hello!");
+//		button.setText("Options");
 	}
 
 	public void setText(String text) {
-		button.setText(text);
+//		button.setText(text);
 	}
 
 	public String getText() {
-		return button.getText();
+		return "";
+//		return button.getText();
 	}
 
 }
