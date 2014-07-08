c68dce22d34bd58bd1a3a4f735bd39e25f0cef91
Anatoly.Kondratyev@gmail.com <Anatoly.Kondratyev@gmail.com@6788df26-0534-eb5a-cd33-def53b161572>
2014-07-08 21:14:16 +0000

    Revert multithreading

--- trunk/src/main/java/roi/students/t3t/shared/service/impl/AgreggationServiceImpl.java
+++ trunk/src/main/java/roi/students/t3t/shared/service/impl/AgreggationServiceImpl.java
@@ -20,65 +20,67 @@ import roi.students.t3t.shared.dao.impl.ServerResponseImpl;
 import roi.students.t3t.shared.service.AgreggationService;
 
 public class AgreggationServiceImpl implements AgreggationService {
-	
+
 	final iTourParser itourParser = new iTourParser();
 	final ParserNeva nevaTravelParser = new ParserNeva();
-	
+
 	public ServerResponse getResult(Request request) {
 		final HotelRequest hotelRequest = request.getHotelRequest();
-		
+
 		//
-		//интерфесы для параллельной обработки инфы с парсеров
-		//добавил 3, т. к. у нас по идее столько парсеров должно быть
+		// интерфесы для параллельной обработки инфы с парсеров
+		// добавил 3, т. к. у нас по идее столько парсеров должно быть
 		//
-		Callable<List<HotelInfo>> callable1 = new Callable<List<HotelInfo>>(){
-			
+		Callable<List<HotelInfo>> callable1 = new Callable<List<HotelInfo>>() {
+
 			@Override
-			public List<HotelInfo> call() throws Exception
-			{
+			public List<HotelInfo> call() throws Exception {
 				return itourParser.getList(hotelRequest);
 			}
 		};
-		Callable<List<HotelInfo>> callable2 = new Callable<List<HotelInfo>>(){
-			
+		Callable<List<HotelInfo>> callable2 = new Callable<List<HotelInfo>>() {
+
 			@Override
-			public List<HotelInfo> call() throws Exception
-			{
+			public List<HotelInfo> call() throws Exception {
 				return nevaTravelParser.getList(hotelRequest);
 			}
 		};
-		/*Callable<List<HotelInfo>> callable3 = new Callable<List<HotelInfo>>(){
-			
-			@Override
-			public List<HotelInfo> call() throws Exception
-			{
-				//TODO: here must be method from third parser
-				return null;
-			}
-		};*/
-		
-		
-		ArrayList<Future<List<HotelInfo>>> resultsAsync = new ArrayList<Future<List<HotelInfo>>>();
-		List<HotelInfo> parserResults = new ArrayList<HotelInfo>();
-		Set<Site> siteSet = request.getClientSettings().getSiteList();
-		ExecutorService exec = Executors.newCachedThreadPool();
-		for(Site site:siteSet){
-			switch(site){
+		/*
+		 * Callable<List<HotelInfo>> callable3 = new
+		 * Callable<List<HotelInfo>>(){
+		 * 
+		 * @Override public List<HotelInfo> call() throws Exception { //TODO:
+		 * here must be method from third parser return null; } };
+		 */
+
+		ArrayList<HotelInfo> result = new ArrayList<HotelInfo>();
+
+		// ArrayList<Future<List<HotelInfo>>> resultsAsync = new
+		// ArrayList<Future<List<HotelInfo>>>();
+		// List<HotelInfo> parserResults = new ArrayList<HotelInfo>();
+	 Set<Site> siteSet = request.getClientSettings().getSiteList();
+		// ExecutorService exec = Executors.newCachedThreadPool();
+		for (Site site : siteSet) {
+			switch (site) {
 			case itour:
-				resultsAsync.add(exec.submit(callable1));
+				result.addAll(itourParser.getList(hotelRequest));
+				break;
+			//	resultsAsync.add(exec.submit(callable1));
 			case teztour:
-//				MockTezTourParser tezTourParser = new MockTezTourParser();
-//				parserResults.addAll(tezTourParser.getList(hotelRequest));
+				// MockTezTourParser tezTourParser = new MockTezTourParser();
+				// parserResults.addAll(tezTourParser.getList(hotelRequest));
 				break;
 			case nevatravel:
-				resultsAsync.add(exec.submit(callable2));
+				result.addAll(nevaTravelParser.getList(hotelRequest));
+				
+				//resultsAsync.add(exec.submit(callable2));
 				break;
 			default:
 				break;
 			}
 		}
-		exec.shutdown();
-		
+	/*	exec.shutdown();
+
 		for (Future<List<HotelInfo>> info : resultsAsync)
 			try {
 				parserResults.addAll(info.get());
@@ -88,7 +90,8 @@ public class AgreggationServiceImpl implements AgreggationService {
 			} catch (ExecutionException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-		return new ServerResponseImpl(AgreggationServiceSuit.getThreeBest(parserResults),request);
+			}*/
+		return new ServerResponseImpl(
+				AgreggationServiceSuit.getThreeBest(result), request);
 	}
 }
