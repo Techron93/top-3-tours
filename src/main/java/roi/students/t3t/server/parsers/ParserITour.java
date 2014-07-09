e81bd5b4c8258edac9c0126b5779ab224bc6da44
sergeevv95@gmail.com <sergeevv95@gmail.com@6788df26-0534-eb5a-cd33-def53b161572>
2014-07-09 07:43:13 +0000

    Added method getBaxPrice and id of country for itour.ru in enum Country. Source code of ParserITour has been rewritten.

---
+++ src/main/java/roi/students/t3t/server/parsers/ParserITour.java
@@ -0,0 +1,133 @@
+package roi.students.t3t.server.parsers;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.xml.XmlPage;
+
+import roi.students.t3t.server.SiteParser;
+import roi.students.t3t.shared.dao.HotelInfo;
+import roi.students.t3t.shared.dao.HotelRequest;
+import roi.students.t3t.shared.dao.impl.HotelInfoImpl;
+
+public class ParserITour implements SiteParser{
+	
+	public ParserITour()
+	{}
+
+	public List<HotelInfo> getList(HotelRequest request) {
+		
+		String url = buildUrl(request);
+		List<HotelInfo> result = new ArrayList<HotelInfo>(0);
+		WebClient webClient = new WebClient(BrowserVersion.FIREFOX_24);
+		webClient.getOptions().setJavaScriptEnabled(true);
+		webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+		webClient.getOptions().setThrowExceptionOnScriptError(false);
+		webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+		
+		HtmlPage page = null;
+		Document doc = null;
+		try
+		{
+			page = webClient.getPage(url);
+			webClient.waitForBackgroundJavaScript(10000);
+			String xmlPage = page.asXml();
+			doc = Jsoup.parse(xmlPage);
+			
+			Elements elements = doc.select("div.tour-group.cfx.ng-scope");
+			for (int i = 0; i < elements.size(); i++)
+			{
+				Elements els_name = elements.get(i).select("div.name.ng-scope");
+				Elements els_stars = elements.get(i).select("div.hotel-stars.ng-binding");
+				Elements els_price = elements.get(i).select("a.alt.ng-scope.ng-binding");
+				Elements els_link= elements.get(i).select("a.btn.btn-xs.btn-green.btn-more");
+				String hyperLink = els_link.get(0).attr("abs:href");
+				
+				StringBuilder priceString = new StringBuilder(els_price.get(0).text());
+				String[] priceM = priceString.substring(2, priceString.length() - 5).split(" ");
+				String priceS = "";
+				for (String temp : priceM)
+					priceS += temp;
+				
+				result.add(new HotelInfoImpl(hyperLink, Integer.parseInt(priceS), els_name.get(0).text(), Integer.parseInt(els_stars.get(0).text())));
+			}
+		}
+		catch (MalformedURLException e)
+		{e.printStackTrace();}
+		catch (IOException e)
+		{e.printStackTrace();}
+		return result;
+	}
+
+	public String buildUrl(HotelRequest request){
+		
+		double mesure = getBaxPrice();
+		int peopleCount = 4;
+		peopleCount = request.getPeopleCount() != 4 ? (request.getPeopleCount() - 1) : peopleCount;
+		int roundedMinPrice = (int) Math.round(request.getMinPrice() / mesure);
+		int roundedMaxPrice = (int) Math.round(request.getMaxPrice() / mesure);
+		
+		StringBuilder url = new StringBuilder();
+		url.append("http://itour.ru/tour/?city=2&room=" + peopleCount);
+		url.append("&childAges[]=14&childAges[]=14&arrivalCountry=" + request.getCountry().itour);
+		url.append("&arrivalCountryCode=" + request.getCountry().idITour);
+		url.append("&nightsFrom=" + request.getMinDuration());
+		url.append("&nightsTo=" + request.getMaxDuration());
+		url.append("&grade=" + (request.getMinStars() - 1));
+		url.append("&meal=" + request.getTypeFood());
+		url.append("&priceType=1&departureFrom=" + request.getStartDate());
+		url.append("&departureTo=" + request.getFinishDate());
+		url.append("&priceFrom=" + roundedMinPrice);
+		url.append("&priceTo=" + roundedMaxPrice);
+		url.append("&mealsBetter=true&gradesBetter=false&currencyCode=RUR&bestHotels=20&limit=20");
+		
+		return url.toString();
+	}
+	
+	
+	//находим курс доллара, используя API Центробанка
+	public static double getBaxPrice(){
+		Number result = null;
+		try
+		{
+	        WebClient webClient = new WebClient(BrowserVersion.FIREFOX_24);
+			webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+			webClient.getOptions().setThrowExceptionOnScriptError(false);
+			
+			String day = (LocalDate.now().getDayOfMonth() < 10 ? "0" : "") + LocalDate.now().getDayOfMonth();
+			String month = (LocalDate.now().getDayOfMonth() < 10 ? "0" : "") + LocalDate.now().getMonthValue();
+			String url = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + day  + "/" + month +"/" + LocalDate.now().getYear();
+			
+			XmlPage page = webClient.getPage(url);
+	        String pageAsString = page.asXml();
+			Document doc = Jsoup.parse(pageAsString);
+			Element el = doc.getElementById("R01235");
+			List<Element> values = el.getElementsByTag("Value");
+		    
+			//автоматически форматируем при помощи заданного шаблона
+			DecimalFormat format = new DecimalFormat("#0.0000");
+		    result = format.parse(values.get(0).text());
+		}
+		catch (IOException e)
+		{e.printStackTrace();}
+		catch (ParseException e)
+		{e.printStackTrace();}
+		
+		return result.doubleValue();
+	}
+}
+
--- src/main/java/roi/students/t3t/shared/Country.java
+++ src/main/java/roi/students/t3t/shared/Country.java
@@ -1,19 +1,21 @@
 package roi.students.t3t.shared;
 
 public enum Country {
-	Turkey("tur","tr"),
-	Egypt("egy","eg"),
-	Cyprus("cyp","cy"),
-	Maldives("mv","mv"),
-	Bulgaria("blg","bg");
+	Turkey("tur","tr", 1),
+	Egypt("egy","eg", 2),
+	Cyprus("cyp","cy", 6),
+	Maldives("mv","mv", 19),
+	Bulgaria("blg","bg", 66);
 	
 	
 	public final String nevatravel;
 	public final String itour;
+	public final int idITour;
 	
-	Country(String nevatravel, String itour){
+	Country(String nevatravel, String itour, int id){
 		this.nevatravel = nevatravel;
 		this.itour = itour;
+		this.idITour = id;
 	}
 
 }
