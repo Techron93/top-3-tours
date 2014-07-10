c787603550798f31c0dae372765512aa0c493838
12Liubeznikov@gmail.com <12Liubeznikov@gmail.com@6788df26-0534-eb5a-cd33-def53b161572>
2014-07-10 13:44:01 +0000

    Codes for findtour in Country enum

--- trunk/src/main/java/roi/students/t3t/shared/Country.java
+++ trunk/src/main/java/roi/students/t3t/shared/Country.java
@@ -1,21 +1,22 @@
 package roi.students.t3t.shared;
 
 public enum Country {
-	Turkey("tur","tr", 1),
-	Egypt("egy","eg", 2),
-	Cyprus("cyp","cy", 6),
-	Maldives("mv","mv", 19),
-	Bulgaria("blg","bg", 66);
-	
+	Turkey("tur","tr", 1, 119),
+	Egypt("egy","eg", 2, 40),
+	Cyprus("cyp","cy", 6, 58),
+	Maldives("mv","mv", 19, 72),
+	Bulgaria("blg","bg", 66, 19);
 	
 	public final String nevatravel;
 	public final String itour;
 	public final int idITour;
+	public final int idTourFind;
 	
-	Country(String nevatravel, String itour, int id){
+	Country(String nevatravel, String itour, int id,int idTF){
 		this.nevatravel = nevatravel;
 		this.itour = itour;
 		this.idITour = id;
+		this.idTourFind=idTF;
 	}
-
 }
+
