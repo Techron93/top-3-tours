789cde4c1609a9c4aff49ff6f8ac9b907b4182a0
spb4el@gmail.com <spb4el@gmail.com@6788df26-0534-eb5a-cd33-def53b161572>
2014-07-11 11:30:26 +0000

    Tourfind parser

--- trunk/src/main/java/roi/students/t3t/shared/Site.java
+++ trunk/src/main/java/roi/students/t3t/shared/Site.java
@@ -3,7 +3,8 @@ package roi.students.t3t.shared;
 public enum Site {
 	teztour("http://www.tez-tour.com/","TEZ tour"), 
 	itour("http://itour.ru/","itour"),
-	nevatravel("http://www.nevatravel.ru","nevatravel");	
+	nevatravel("http://www.nevatravel.ru","nevatravel"),
+	tourfind("http://http://www.tour-find/","tourfind");
 	
 	public final String url;
 	public final String name;
